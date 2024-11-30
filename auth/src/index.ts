4649e47e4af4d9d7ed339c9824da408001059bb9
Priyesh Shetty <shettypriyesh761@gmail.com>
2024-12-01 00:18:39 +0530

    k8s + skaffold setup for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express, { json } from "express";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (_, res) => {
+  res.send("get current user");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000...");
 });
