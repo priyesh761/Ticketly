90146f417076b0a5550d931193bdecb30b0897d6
Priyesh Shetty <shettypriyesh761@gmail.com>
2024-12-07 23:12:12 +0530

    Validate + sanitize request params
    - Used express-validator library to validate and
      sanitize request params.
    - express-validator can be used on to process
      body/path/ query params.
    - Used request body validator middleware to
      validate email and password fields for request
      params.
    - express-validator middleware adds validation
      errors in error field in the request object.
    - validationResult method from express-validator
      then processes these error fields to get list of
      errors which is further used to send error
      response.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,24 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
-router.post("/api/users/signup", (_, res) => {
-  res.send("get current user");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().isLength({ min: 5, max: 10 }).withMessage("Password must be between 5 and 10 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send(errors.array());
+      return;
+    }
+    const { email, password } = req.body;
+
+    res.status(201).send(`Creating a user - ${email} ${password}`);
+  }
+);
 
 export { router as signupRouter };
