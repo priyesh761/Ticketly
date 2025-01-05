ec18e4c74d3da94318636ebda28ea58b15b40876
Priyesh Shetty <shettypriyesh761@gmail.com>
2025-01-05 19:03:26 +0530

    Implementing current user and sign-out routes
    - React application cannot directly look into cookie and find if our
      user is logged-in as cookies are htmlOnly cookies (JS cannot access
      htmlOnly cookie). So we need get-current-user route for react
      application to figure out current user in future cases.
    - Current user flow -
        check if req.session.jwt is set, if not return null ->
    	    verify if jwt token in cookie is tampered, if yes return null ->
    		    return payload decrypted from verified JWT.
    - To sign-out user we will simply set req.session = null and then
      cookie-session will handle sending response with no JWT in cookie,
      which will also remove JWT token from cookie in browser.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-router.get("/api/users/currentuser", (_, res) => {
-  res.send("get current user");
+router.get("/api/users/currentuser", (req, res) => {
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null });
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,8 +2,9 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/api/users/signout", (_, res) => {
-  res.send("get current user");
+router.post("/api/users/signout", (req, res) => {
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
