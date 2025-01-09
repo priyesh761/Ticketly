cb6a9ffc9c61556a5d7f79ff15a1381556bae674
Priyesh Shetty <shettypriyesh761@gmail.com>
2025-01-10 00:23:09 +0530

    Global css + SignUp pages
    - Global css in Next needs to be in pages/_app.js file in which we have
      thin wapper for pages we have to show in the screen. By default next
      wraps pages in its own wrapper component.
      More info - https://nextjs.org/docs/messages/css-global
    - Built working prototype for signup page.

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group m-1">
+        <label htmlFor="email">Email</label>
+        <input
+          className="form-control"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          value={email}
+        />
+      </div>
+      <div className="form-group m-1">
+        <label htmlFor="password">Password</label>
+        <input
+          className="form-control"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          value={password}
+        />
+      </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops....</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
