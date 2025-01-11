a498b15083af04790faf6b6e61a45a7302e93a45
Priyesh Shetty <shettypriyesh761@gmail.com>
2025-01-11 23:49:24 +0530

    Custom hook for useRequest
    - Moved api request logic inside a custom hook useRequest as request logic is going to be duplicated across multiple pages.
           _ url			     - doRequest -> Function to execute request
    input-|_ method -> useRequest hook -[
          |_ body			     - errors -> JSX containing errors (if any) after request was made.
          |_ onSuccess		     - on success callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,21 +1,19 @@
+import Router from "next/router";
 import { useState } from "react";
-import axios from "axios";
+import useRequest from "../../hooks/use-request";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-  const onSubmit = async (e) => {
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: { email, password },
+    onSuccess: () => Router.push("/"),
+  });
+  const onSubmit = (e) => {
     e.preventDefault();
-    try {
-      const response = await axios.post("/api/users/signup", {
-        email,
-        password,
-      });
-      console.log(response.data);
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
+    doRequest();
   };
   return (
     <form onSubmit={onSubmit}>
@@ -40,16 +38,7 @@ export default () => {
           value={password}
         />
       </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops....</h4>
-          <ul className="my-0">
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
