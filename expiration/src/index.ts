41bdd8cf1b78b6a6e292152ca23afc1d81f07825
Priyesh Shetty <shettypriyesh761@gmail.com>
2025-03-28 23:35:07 +0530

    Setup expiration service
    Responsibilities
    - Listens to: order:created events.
    - Publishes: expiration:complete events after expiration time.
    - It does not cancel orders directly. Only notifies the Order Service
      that an order's expiration time has passed. Order Service will
      determine whether to cancel (e.g payment was made before expiration).
    
    Expiration Service - Timer Implementation Strategies
    Option 1: setTimeout
    - Use setTimeout() for 15 minutes after receiving order:created.
    - Timers are stored in-memory. If the expiration service restarts,
      all timers are lost.
    
    Option 2: Re-delivery via NATS Streaming
    - Do not acknowledge the order:created event if the expiresAt is in the
      future. Let NATS re-deliver every 5 seconds. On each delivery, check
      expiresAt. If it's in the past, emit expiration:complete and ack.
    - Abuses NATS re-delivery mechanism and can conflict with error tracking
      logic (e.g., redelivery count).
    
    Option 3: Scheduled Event via Event Bus
    - Immediately publish expiration:complete with a 15-minute delay via the
      message broker.
    - Requires scheduled message support. NATS Streaming does not support
      scheduled messages.
    
    Option 4 (Chosen Approach): Bull + Redis
    - Bull.js: Job queue library
    - Redis: In-memory data store for persisting job state
    - On order:created, add a delayed job to Bull (via Redis) to run in 15
      minutes. When job is executed, publish expiration:complete.
    
    - Added setup for expiration service and also set up k8s deployment for
      expiration service as well as redis server.

---
+++ expiration/src/index.ts
@@ -0,0 +1,26 @@
+import { natsWrapper } from "./nats-wrapper";
+
+const KEYS = ["NATS_URL", "NATS_CLUSTER_ID", "NATS_CLIENT_ID"];
+
+const start = async () => {
+  if (!KEYS.every((key) => process.env[key])) {
+    throw new Error("ENV variables must be defined");
+  }
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
