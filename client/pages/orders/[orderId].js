3a12744d3827e536aefcc4459d09328c774263ea
Priyesh Shetty <shettypriyesh761@gmail.com>
2025-07-22 23:02:41 +0530

    Display expiration timer in order details page

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+
 const OrderDetails = ({ order }) => {
-  return <div>Order Detail</div>;
+  const [timeLeft, setTimeLeft] = useState("0");
+
+  useEffect(() => {
+    const calculateTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+    calculateTimeLeft();
+    const timer = setInterval(calculateTimeLeft, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  if (timeLeft <= 0) {
+    return <div>Order has expired</div>;
+  }
+  return <div>Time left to pay: {timeLeft} seconds</div>;
+};
+
+OrderDetails.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+  return { order: data };
 };
 
 export default OrderDetails;
