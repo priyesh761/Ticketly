6483657af2c4262816ad9fd6403734e89d5e3345
Priyesh Shetty <shettypriyesh761@gmail.com>
2025-09-11 00:11:00 +0530

    Update base url for axios client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,8 @@ export default ({ req }) => {
   if (typeof window == "undefined") {
     // On Server
     return axios.create({
-      baseURL: "http://www.ticketly.priyesh-shetty.dev/",
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
